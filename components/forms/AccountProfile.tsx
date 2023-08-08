f305b72501431f03a0a0ca9ab803380cd9910985
Rendi Purnomo <107144802+rendipurnomo@users.noreply.github.com>
2023-08-08 19:51:26 +0700

    setting mongoose

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -18,6 +18,8 @@ import { ChangeEvent, useState } from 'react';
 import { Textarea } from '../ui/textarea';
 import { isBase64Image } from '@/lib/utils';
 import { useUploadThing } from '@/lib/uploadthing';
+import { updateUser } from '@/lib/actions/user.actions';
+import { useRouter, usePathname } from 'next/navigation';
 
 
 interface Props {
@@ -35,6 +37,8 @@ interface Props {
 const AccountProfile = ({user, btnTitle} : Props ) => {
   const [files, setFiles]=useState<File[]>([])
   const { startUpload } = useUploadThing("media");
+  const router = useRouter();
+  const pathname = usePathname();
 
         // 1. Define your form.
     const form = useForm({
@@ -81,8 +85,22 @@ const AccountProfile = ({user, btnTitle} : Props ) => {
         values.profile_photo = imgRes[0].fileUrl;
       }
     }
-  }
 
+    await updateUser({
+      userId:user.id,
+      username: values.username,
+      name: values.name,
+      bio: values.bio,
+      image:values.profile_photo,
+      path: pathname
+    });
+
+    if(pathname === '/profile/edit') {
+      router.back();
+    }else {
+      router.push('/');
+    }
+  }
 
   return (
     <Form {...form}>
