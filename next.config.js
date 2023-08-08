aa2c45a5f4c641702c5ec72ceb37e8bc552552cc
Rendi Purnomo <107144802+rendipurnomo@users.noreply.github.com>
2023-08-08 15:09:11 +0700

    create validation

--- next.config.js
+++ next.config.js
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        serverActions:true,
+        serverComponentsExternalPackages: ['mongoose'],
+
+    },
+    images: {
+        remotePatterns: [
+        {
+            protocol: 'https',
+            hostname: 'img.clerk.com'
+        },
+        {
+            protocol: 'https',
+            hostname: 'img.clerk.dev'
+        },
+        {
+            protocol: 'https',
+            hostname: 'uploading.com'
+        },
+        {
+            protocol: 'https',
+            hostname: 'placehold.co'
+        },
+        ]
+    }
+}
 
 module.exports = nextConfig
